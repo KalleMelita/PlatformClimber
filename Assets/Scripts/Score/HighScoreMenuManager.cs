53ac63bf8b03316a66ce3aae69b5299639292e73
KalleMelita <kallemelita@googlemail.com>
2023-04-16 19:57:24 +0200

    Refactoring: add comments

--- Assets/Scripts/Score/HighScoreMenuManager.cs
+++ Assets/Scripts/Score/HighScoreMenuManager.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// Scripts for a High Score Menue Manager object.
+/// </summary>
 public class HighScoreMenuManager : MonoBehaviour
 {
-    Canvas canvas;
+    /// <summary>
+    /// Vertiacal Layout Group  that contains UI-Elemnts to display High Scores.
+    /// </summary>
     public GameObject VerticalLayoutGroupGO;
-    
 
+    /// <summary>
+    /// Clear all High Scores in the UI on Start.
+    /// </summary>
     void Start()
     {
-        canvas = GameObject.FindWithTag(TagEnum.CANVAS).GetComponent<Canvas>();
         Clear();
     }
 
+    /// <summary>
+    /// Builds the High Score.
+    /// Gets all High Scores form the Save System.
+    /// Sorts the High Scores an runs throw the 10 best scores.
+    /// Finds the 10 Layout Groups to display the High Scores.
+    /// The Layout groups need to be names after the pattern 'LayoutGroup (<Score Ranking>)'.
+    /// So the Layhout group for Score 5 must be named 'LayoutGroup (5)'.
+    /// </summary>
     public void BuildHighScore()
     {
         List<PlayerHighScore> playerHighScores = SaveSystem.readPlayerHighScore();
         playerHighScores.Sort();
         playerHighScores.Reverse();
 
-        float height = 100;
-
-        float scale = canvas.scaleFactor;
-
         for (int i = 1; i < 11 && i < playerHighScores.Count; i++)
         {
-            Transform layoutGroupTransform = VerticalLayoutGroupGO.transform.Find(string.Format("LayoutGroup ({0})",i));
+            Transform layoutGroupTransform = VerticalLayoutGroupGO.transform.Find(string.Format("LayoutGroup ({0})", i));
 
             Transform playerNameT = layoutGroupTransform.transform.Find("PlayerName");
             TextMeshProUGUI playerText = playerNameT.GetComponent<TextMeshProUGUI>();
@@ -40,11 +50,18 @@ public class HighScoreMenuManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Destroy/ clear the High Score.
+    /// All Score displaying Elements will be resettet.
+    /// </summary>
     public void DestroyHighScore()
     {
         Clear();
     }
 
+    /// <summary>
+    /// Runs through all displaying Elements and resets them.
+    /// </summary>
     private void Clear()
     {
         for (int i = 1; i < 11; i++)
